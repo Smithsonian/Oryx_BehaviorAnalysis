356636e2f1630518b22044ef8d23abd98b9b709d
connetteg <39745925+connetteg@users.noreply.github.com>
2018-05-30 14:33:00 -0400

    Got rid of the betas to simplify the model.  Also simplified the derived quantities section.

--- Multinomial_NewFormulation.R
+++ Multinomial_NewFormulation.R
@@ -1,20 +1,12 @@
 model{
   
   #  PRIORS
-  alpha[1] <- 0;       # zero contrast for baseline activity
-  for (j in 2:n.outcomes) {   # loop over activities
-    alpha[j] ~ dnorm(0, 0.001)
-  }
-  
-  for (j in 1:n.outcomes){  
-    beta[1, j] <- 0   # zero contrast for control treatment
-  }
-  
-  for (i in 2:3) {    # loop over treatments
-    beta[i, 1] <- 0   # zero contrast for baseline activity
-    for (j in 2:n.outcomes){ 
-      beta[i, j] ~ dnorm(0, 0.001)
-    } 
+  for (i in 1:3){
+    alpha[i,1] <- 0;   # Here we are fixing the relative probability of the baseline behavior to zero for all time periods (pre/post collar)
+
+    for (j in 2:n.outcomes) {   # loop over response behaviors (HU, HD, Laying, Headshake, etc...)
+      alpha[i,j] ~ dnorm(0, 0.001)   # Here we are assigning diffuse normal priors to the relative probabilities of all behaviors except the reference
+    }
   }
   
   # LIKELIHOOD  
@@ -24,17 +16,10 @@ model{
 
     for (j in 1:n.outcomes) {     # loop around
       p[i,j] <- phi[i,j] / sum(phi[i, ])
-      log(phi[i,j]) <- alpha[j] + beta[PERIOD[i], j]
+      log(phi[i,j]) <- alpha[period[i],j]
     }
   }
   
   # Derived quantities
-  for (j in 1:n.outcomes){
-    PROBS[1,j] <- PHI[1,j] / sum(PHI[1,])
-    log(PHI[1,j]) <- alpha[j] + beta[1,j]
-    PROBS[2,j] <- PHI[2,j] / sum(PHI[2,])
-    log(PHI[2,j]) <- alpha[j] + beta[2,j]
-    PROBS[3,j] <- PHI[3,j] / sum(PHI[3,])
-    log(PHI[3,j]) <- alpha[j] + beta[3,j]
-  }
+  PROBS <- p   # We actually don't need to re-calculate these, we could just add p to the parameters to monitor or just save this as 'PROBS'
 }
